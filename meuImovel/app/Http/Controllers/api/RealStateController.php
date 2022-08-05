754df9165080b496a9e4ff8534d5276b6f699456
Cesar A. Pires <cesar.augusto.pires@hotmail.com>
2022-08-05 14:56:43 -0300

    Adicionando o primeiro endpoint getAllRealState, Model/RealState, RealStateController, route/api

---
+++ meuImovel/app/Http/Controllers/api/RealStateController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\RealState;
+use Illuminate\Http\Request;
+
+class RealStateController extends Controller{
+    private $realState;
+
+    public function __construct(RealState $realState)
+    {
+        $this->realState = $realState;
+    }
+
+    public function index() {
+        $realState = $this->realState->paginate(10);
+        return response()->json($realState, 200);
+    }
+}
